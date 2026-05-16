92072d7600f37748c14d91d528acef64aaffd2c8
Carlos Daniel Vilaseca Illnait <carlosvilaseca406@gmail.com>
2026-05-16 15:57:08 -0400

    fix: server env schema

--- env.server.ts
+++ env.server.ts
@@ -5,7 +5,7 @@ import { z } from 'zod'
 
 export const serverEnv = z
   .object({
-    NODE_ENV: z.literal(['development', 'production']).default('development'),
+    NODE_ENV: z.enum(['development', 'production']).default('development'),
 
     CLERK_WEBHOOK_SECRET: z.string(),
     CLERK_WEBHOOK_SIGNING_SECRET: z.string(),
@@ -16,7 +16,7 @@ export const serverEnv = z
   })
   .refine(
     ({ NODE_ENV, NGROK_AUTH_TOKEN, NGROK_URL }) =>
-      NODE_ENV == 'development' && NGROK_AUTH_TOKEN && NGROK_URL,
+      NODE_ENV !== 'development' || (!!NGROK_AUTH_TOKEN && !!NGROK_URL),
     { message: 'In development, NGROK variables are required' },
   )
   .parse(process.env)
